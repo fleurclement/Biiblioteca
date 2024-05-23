1e235ab5bc7b4028f82a39b640e0ccc716cf9f44
fleurclement <fleurclement4@gmail.com>
2024-05-22 23:39:59 -0400

    feat/ creamos componentes

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,45 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import Narbar from './component/Narbar';
+import BookCard from './component/BookCard';
 
 function App() {
-  const [count, setCount] = useState(0)
+const libros = [ 
+  {
+    imagen:"https://picsum.photos/seed/picsum/200/300",
+    title: "Titulo1",
+    description: "Descripcion1"
+  },
+
+  {
+    imagen:"https://picsum.photos/seed/picsum/200/300",
+    title: "Titulo2",
+    description: "Descripcion2"
+  },
+  {
+    imagen:"https://picsum.photos/seed/picsum/200/300",
+    title: "Titulo3",
+    description: "Descripcion3"
+  }
+
+] 
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <Narbar title="Biblioteca de Libros " /> 
+      <div className='container mt-3'>
+        <div className='row'>
+          {
+            libros.map((libro,index) => (
+              <div className='col-md-4'
+              key={index}
+              >
+                <BookCard {...libro}/>
+              </div>
+            ))
+          }
+
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
---
+++ src/component/Narbar.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Narbar = ({title}) => {
+  return (
+    <div>
+        <h1>{title} </h1>
+    </div>
+  )
+}
+
+export default Narbar
+
